d72cc333098b32f05aa2f5d2c4c6aaa944544232
canumut05 <hasozdemirumutcan@gmail.com>
2023-12-28 20:59:39 +0300

    Proje dosyası ekle.

---
+++ rasgeleatama/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a, sayi;
+
+        string[] b = { "umut", "hazar", "serdar", "arif" };
+        string[] c = { "istanbul", "amerika", "ingiltere", "dubai" };
+        bool[] d = new bool[4];
+
+        Random rastgele = new Random();
+
+        for (a = 0; a < 4; ++a)
+        {
+            do
+            {
+                sayi = rastgele.Next(0, 4);
+            }
+            while (d[sayi] == true);
+            d[sayi] = true;
+
+            Console.WriteLine(b[a] + " = " + c[sayi]);
+        }
+    }
+}
